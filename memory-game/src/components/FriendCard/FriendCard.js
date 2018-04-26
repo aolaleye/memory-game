4dd63bf43c0abe0374e14b20dddbe190a2f15ff3
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-04-26 01:29:36 -0400

    changed FriendCard component to just display images

--- memory-game/src/components/FriendCard/FriendCard.js
+++ memory-game/src/components/FriendCard/FriendCard.js
@@ -2,26 +2,10 @@ import React from "react";
 import "./FriendCard.css";
 
 const FriendCard = props => (
-  <div className="card">
-    <div className="img-container">
-      <img alt={props.name} src={props.image} />
+  <div className="col-2 m-5">
+    <div className="img-fluid pointer">
+      <img alt={props.name} src={props.image} onClick={() => props.shuffle(props.id)}/>
     </div>
-    <div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    <span onClick={() => props.removeFriend(props.id)} className="remove">
-      𝘅
-    </span>
   </div>
 );
 
